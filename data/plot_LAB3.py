9c4c22b5bb31c699295704b627e5f88a85ecee9a
Vemund Rogne <vemundrogne@gmail.com>
2021-03-30 16:56:54 +0200

    Add LAB3-R plots

--- data/plot_LAB3.py
+++ data/plot_LAB3.py
@@ -3,22 +3,34 @@ import matplotlib.pyplot as plt
 import optlab
 
 
-R_tests = [
-    optlab.open_mat("LAB3/"+path, LABn=3)
-    for path in [
-        "data_R0.010.mat",
-        "data_R0.100.mat",
-        "data_R1.000.mat",
-        "data_R10.000.mat"
+def plot_different_R_values():
+    R_tests = [
+        optlab.open_mat("LAB3/"+path, LABn=3)
+        for path in [
+            "data_R0.010.mat",
+            "data_R0.100.mat",
+            "data_R1.000.mat",
+            "data_R10.000.mat"
+        ]
     ]
-]
-R_tests_labels = ["R=0.01", "R=0.10", "R=1.0", "R=10"]
+    R_tests_labels = ["R=0.01", "R=0.10", "R=1.0", "R=10"]
 
-optlab.plot.plot_comparisons(
-    R_tests,
-    R_tests_labels,
-    ['Travel', 'Travelrate', 'Pitch', 'Pitchrate', 'u'],
-    plot_optimal_trajectory=True
-)
+    fig, ax = optlab.plot.plot_comparisons(
+        R_tests,
+        R_tests_labels,
+        ['Travel', 'Travelrate', 'Pitch', 'Pitchrate', 'u'],
+        plot_optimal_trajectory=True,
+        xlim=[0, 25],
+        legend_loc='right'
+    )
 
-plt.show()
+    fig.set_size_inches(6.5, 9)
+
+    return fig, ax
+
+
+if __name__ == '__main__':
+    optlab.enable_pgf_plots()
+    fig, ax = plot_different_R_values()
+    fig.suptitle("Effect of varying R-values in the LQR regulator")
+    optlab.plot.export_plot("LAB3_R_variations")
