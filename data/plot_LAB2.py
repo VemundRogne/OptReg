cff089b3c2ab0015f04a2c198cba8843171dda31
Vemund Rogne <vemundrogne@gmail.com>
2021-04-06 12:52:30 +0200

    Improve LAB2 plot
    - Add optimal trajectory to the plot
    - Add the flight without setpoints to show drift irrelevant of control
    
    Unfortunately not PGF, we are using png for now...

--- data/plot_LAB2.py
+++ data/plot_LAB2.py
@@ -3,25 +3,40 @@ import optlab
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
-matplotlib.rcParams['axes.unicode_minus'] = False
+#matplotlib.use("pgf")
+#matplotlib.rcParams.update({
+#    "pgf.texsystem": "pdflatex",
+#    'font.family': 'serif',
+#    'text.usetex': True,
+#    'pgf.rcfonts': False,
+#})
+#matplotlib.rcParams['axes.unicode_minus'] = False
+
+def add_opt(df):
+    df_with_opt = optlab.open_mat("LAB3/data_R0.010.mat", LABn=3)
+
+    for col in ["Travel_opt", "Pitch_opt"]:
+        df[col] = df_with_opt[col]
+    return df
 
 def plot_1():
     df = optlab.open_mat("LAB2/lab2_without_pitch_offset.mat", LABn=2)
-    fig, ax = optlab.plot.plot_flight(
-        df,
-        columns = ['Travel', 'Pitch'],
+    df = add_opt(df)
+
+    df_2 = optlab.open_mat("LAB2/lab2_without_pitch_setpoints.mat", LABn=2)
+    df_2 = add_opt(df_2)
+
+    fig, ax = optlab.plot.plot_comparisons(
+        [df, df_2],
+        labels = ["Flight with optimal setpoints", "Flight without setpoints"],
+        columns_to_compare = ['Travel', 'Pitch'],
         xlim=(0, 30),
-        ylims=[[0, 5], None]
+        #ylims=[[0, 5], None],
+        plot_optimal_trajectory=True
     )
     fig.suptitle("Optimal control of pitch/travel without feedback")
     optlab.plot.export_plot("LAB2_plot_1")
 
 if __name__ == '__main__':
     plot_1()
+    #plt.show()
